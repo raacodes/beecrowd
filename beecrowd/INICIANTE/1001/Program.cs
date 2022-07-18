1b6324ca7d003097f02b87f8b3db8f5e52838b9f
Renato Alisson <poleguin@gmail.com>
2022-07-18 19:57:28 -0300

    1001

---
+++ beecrowd/INICIANTE/1001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Beecrowdxercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B;
+        
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+        
+            Console.WriteLine($"X = {A+B}");
+        }
+    }
+}
