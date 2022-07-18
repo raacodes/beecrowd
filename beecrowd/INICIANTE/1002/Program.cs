0e03ea7e7e8df1c5197fe7f209f23e606b39c1fc
Renato Alisson <poleguin@gmail.com>
2022-07-18 20:04:44 -0300

    exercicios bee

---
+++ beecrowd/INICIANTE/1002/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BeecrowdExercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                double n = 3.14159;
+                double raio;
+
+                raio = double.Parse(Console.ReadLine());
+
+                double A = (Math.Pow(raio, 2)) * n;
+
+                Console.WriteLine($"A={A:F4}");
+        }
+    }
+}
---
+++ beecrowd/INICIANTE/1004/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BeecrowdExercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = double.Parse(Console.ReadLine());
+            double B = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"PROD = {A*B}");
+        }
+    }
+}
